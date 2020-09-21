eae21dcb871a34693597ac38ddb4847d5ae207cc
LAURA JELENICH <laurajelenich@LAURAs-MBP-2.lan1>
2020-09-21 16:34:31 -0400

    final

---
+++ SWAPI.playground/Contents.swift
@@ -0,0 +1,89 @@
+import UIKit
+
+struct Person: Decodable {
+    let name: String
+    let films: [URL]
+}
+
+struct Film: Decodable {
+    let title: String
+    let opening_crawl: String
+    let release_date: String
+}
+
+class SwapiService {
+    static let baseURL = URL(string: "https://swapi.dev/api/")
+    static let peopleEndpoint = "people/"
+    
+    static func fetchPerson(id: Int, completion: @escaping (Person?) -> Void) {
+      // 1 - Prepare URL
+        guard let baseURL = baseURL else { return completion(nil)}
+        let peopleURL = baseURL.appendingPathComponent(peopleEndpoint)
+        let finalURL = peopleURL.appendingPathComponent("\(id)")
+        print(finalURL)
+      // 2 - Contact server
+        URLSession.shared.dataTask(with: finalURL) { (data, _, error) in
+            // 3 - Handle errors
+            if let error = error {
+                print(error)
+                print(error.localizedDescription)
+                return completion(nil)
+            }
+            // 4 - Check for data
+            guard let data = data else { return completion(nil) }
+
+            // 5 - Decode Person from JSON
+            do {
+                let decoder = JSONDecoder()
+                let person = try decoder.decode(Person.self, from: data)
+                return completion(person)
+            } catch {
+                print(error)
+                print(error.localizedDescription)
+                return completion(nil)
+            }
+        }.resume()
+    }
+    
+    static func fetchFilm(url: URL, completion: @escaping (Film?) -> Void) {
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            guard let data = data else {return completion(nil) }
+            
+            do {
+                let decoder = JSONDecoder()
+                let film = try decoder.decode(Film.self, from: data)
+                return completion(film)
+            } catch {
+                print(error)
+                print(error.localizedDescription)
+                return completion(nil)
+            }
+            
+        }.resume()
+ 
+    }
+}
+
+func fetchFilm(url: URL) {
+  SwapiService.fetchFilm(url: url) { film in
+    if let film = film?.title {
+        print("Title: \(film)")
+    }
+    if let film = film?.release_date {
+        print("Release Date: \(film)")
+    }
+    if let film = film?.opening_crawl {
+        print("Opening Crawl: \(film)")
+    }
+  }
+}
+
+SwapiService.fetchPerson(id: 1) { person in
+  if let person = person {
+    print("Name: \(person.name)")
+    for film in person.films {
+        fetchFilm(url: film)
+    }
+  }
+}
+
